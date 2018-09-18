3f852470446853d60c7ddffb819c6bba930b4b80
TaeHoKim <taeh033.kim@gmail.com>
2018-09-17 17:11:30 -0700

    implemented east traversal

--- reversi.py
+++ reversi.py
@@ -17,7 +17,7 @@ class Reversi():
 
     def move(self, position):
         letter_index = self.__letter_to_index(position[0])
-        number_index = int(position[1]) - 1
+        number_index = self.__number_to_index(position[1])
         self.board[number_index][letter_index] = self.player
 
         if self.player == 'black':
@@ -26,6 +26,25 @@ class Reversi():
             self.player = 'black'
 
         return True
+
+    def does_east_traversal_allow_valid_move(self, color, position):
+        letter_index = self.__letter_to_index(position[0])
+        number_index = self.__number_to_index(position[1])
+
+        opposite_color_count = 0
+        for i in range(letter_index+1, 8):
+            if self.player == self.board[number_index][i]:
+                break
+            else:
+                opposite_color_count += 1
+        if self.player == self.board[number_index][i] and \
+            opposite_color_count > 1:
+            return True
+
+        return False
+
+    def __number_to_index(self, number):
+        return int(number) - 1
 
     def __letter_to_index(self, letter):
         return ord(letter.lower()) - 97
@@ -68,7 +87,32 @@ class TestReversi(unittest.TestCase):
         ]
         self.assertEqual(expected, r.board)
 
-    def test_move_is_placed(self):
+    def test_does_east_traversal_allow_valid_move(self):
+        r = Reversi()
+        r.board = [
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, 'black', None, None, None, None],
+        [None, None, None, 'black', 'black', None, None, None],
+        [None, None, 'white', 'white', 'black', None, None, None],
+        [None, None, None, None, 'black', None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None]
+        ]
+        self.assertTrue(r.does_east_traversal_allow_valid_move('black', 'B5'))
+
+        r = Reversi()
+        r.board = [
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, 'black', None, None, None, None],
+        [None, None, None, 'black', 'black', None, None, None],
+        [None, None, 'white', 'white', 'white', None, None, None],
+        [None, None, None, None, 'black', None, None, None],
+        [None, None, None, None, None, None, None, None],
+        [None, None, None, None, None, None, None, None]
+        ]
+        self.assertFalse(r.does_east_traversal_allow_valid_move('black', 'B5'))
         
 
 if __name__ == '__main__':
