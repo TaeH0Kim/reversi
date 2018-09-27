08580d98d4023f20c5b9883ccca2fd0f42dfd223
Eric Mathison <dev@ericmathison.io>
2018-09-27 16:02:53 -0700

    Use string argument for board initialization
    This makes reading the board state more intuitive and obvious. Visually
    parsing the board state as an array was confusing because values had
    different lengths on the screen.

--- reversi.py
+++ reversi.py
@@ -3,221 +3,196 @@ import unittest
 
 class Reversi():
 
-    def __init__(self):
-        self.board = [
-            [None] * 8,
-            [None] * 8,
-            [None] * 8,
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None] * 8,
-            [None] * 8,
-            [None] * 8
-        ]
-        self.player = 'black'
+    def __init__(self, board_str='''
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - w b - - -
+            - - - b w - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            '''):
+        board = {
+            'A': {}, 'B': {}, 'C': {}, 'D': {},
+            'E': {}, 'F': {}, 'G': {}, 'H': {}
+            }
+        rows = board_str.strip().split("\n")
+        board_ary = [i.strip().split(' ') for i in rows]
+        for i, row in enumerate(board_ary):
+            for j, cell in enumerate(row):
+                column_letter = chr(j + 65)
+                board[column_letter][i + 1] = cell
+        self.board = board
+        self.player = 'b'
 
     def move(self, position):
-        letter_index = self.__letter_to_index(position[0])
-        number_index = self.__number_to_index(position[1])
-        self.board[number_index][letter_index] = self.player
+        column = position[0]
+        row = int(position[1])
 
-        if self.player == 'black':
-            self.player = 'white'
+        self.board[column][row] = self.player
+
+        if self.player == 'b':
+            self.player = 'w'
         else:
-            self.player = 'black'
+            self.player = 'b'
 
         return True
 
     def does_east_traversal_allow_valid_move(self, color, position):
-        letter_index = self.__letter_to_index(position[0])
-        number_index = self.__number_to_index(position[1])
+        column = position[0]
+        column_index = ord(column) - 65
+        row = int(position[1])
 
-        opposite_color_count = 0
-
-        if letter_index >= 7:
+        if column_index >= 7:
             return False
 
-        for i in range(letter_index+1, 8):
-            if self.player == self.board[number_index][i]:
+        opposite_color_count = 0
+        for i in range(column_index+1, 8):
+            if color == self.board[chr(i + 65)][row]:
                 break
             else:
                 opposite_color_count += 1
-        if (self.player == self.board[number_index][i] and
-                opposite_color_count >= 1):
+        if color == self.board[chr(i + 65)][row] and opposite_color_count >= 1:
             return True
 
         return False
 
     def does_west_traversal_allow_valid_move(self, color, position):
-        letter_index = self.__letter_to_index(position[0])
-        number_index = self.__number_to_index(position[1])
+        column = position[0]
+        column_index = ord(column) - 65
+        row = int(position[1])
 
-        if letter_index <= 0:
+        if column_index <= 0:
             return False
 
         opposite_color_count = 0
-        for i in range(letter_index-1, -1, -1):
-            if self.player == self.board[number_index][i]:
+        for i in range(column_index-1, -1, -1):
+            if color == self.board[chr(i + 65)][row]:
                 break
-            elif self.board[number_index][i] is not None:
+            elif self.board[chr(i + 65)][row] != '-':
                 opposite_color_count += 1
 
-        if (self.player == self.board[number_index][i] and
-                opposite_color_count >= 1):
+        if color == self.board[chr(i + 65)][row] and opposite_color_count >= 1:
             return True
 
         return False
-
-    def __number_to_index(self, number):
-        return int(number) - 1
-
-    def __letter_to_index(self, letter):
-        return ord(letter.lower()) - 97
 
 
 class TestReversi(unittest.TestCase):
 
     def test_move_changes_player(self):
         r = Reversi()
-        self.assertEqual(r.player, 'black')
+        self.assertEqual(r.player, 'b')
         r.move("A1")
-        self.assertEqual(r.player, 'white')
+        self.assertEqual(r.player, 'w')
         r.move("A2")
-        self.assertEqual(r.player, 'black')
+        self.assertEqual(r.player, 'b')
 
     def test_move_places_piece_on_board(self):
         r = Reversi()
         r.move('A1')
-        expected = [
-            ['black', None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertEqual(expected, r.board)
-
+        self.assertEqual(r.board['A'][1], 'b')
         r.move('B7')
-        expected = [
-            ['black', None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, 'white', None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertEqual(expected, r.board)
+        self.assertEqual(r.board['B'][7], 'w')
 
     def test_does_east_traversal_allow_valid_move(self):
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'black', None, None, None, None],
-            [None, None, None, 'black', 'black', None, None, None],
-            [None, None, 'white', 'white', 'black', None, None, None],
-            [None, None, None, None, 'black', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertTrue(r.does_east_traversal_allow_valid_move('black', 'B5'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - b - - - -
+            - - - b b - - -
+            - - w w b - - -
+            - - - - b - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertTrue(r.does_east_traversal_allow_valid_move('b', 'B5'))
 
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'black', None, None, None, None],
-            [None, None, None, 'black', 'black', None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, None, 'black', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertTrue(r.does_east_traversal_allow_valid_move('black', 'C5'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - b - - - -
+            - - - b b - - -
+            - - - w b - - -
+            - - - - b - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertTrue(r.does_east_traversal_allow_valid_move('b', 'C5'))
 
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'black', None, None, None, None],
-            [None, None, None, 'black', 'black', None, None, None],
-            [None, None, 'white', 'white', 'white', None, None, None],
-            [None, None, None, None, 'black', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertFalse(r.does_east_traversal_allow_valid_move('black', 'B5'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - b - - - -
+            - - - b b - - -
+            - - w w w - - -
+            - - - - b - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertFalse(r.does_east_traversal_allow_valid_move('b', 'B5'))
 
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'black', None, None, None, None],
-            [None, None, None, 'black', 'black', None, None, None],
-            [None, None, 'white', 'white', 'white', None, None, None],
-            [None, None, None, None, 'black', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertFalse(r.does_east_traversal_allow_valid_move('black', 'H2'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - b - - - -
+            - - - b b - - -
+            - - w w w - - -
+            - - - - b - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertFalse(r.does_east_traversal_allow_valid_move('b', 'H2'))
 
     def test_does_west_traversal_allow_valid_move(self):
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertTrue(r.does_west_traversal_allow_valid_move('black', 'F5'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - w b - - -
+            - - - b w - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertTrue(r.does_west_traversal_allow_valid_move('b', 'F5'))
 
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertFalse(r.does_west_traversal_allow_valid_move('black', 'F4'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - w b - - -
+            - - - b w - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertFalse(r.does_west_traversal_allow_valid_move('b', 'F4'))
 
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertFalse(r.does_west_traversal_allow_valid_move('black', 'A8'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - w b - - -
+            - - - b w - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertFalse(r.does_west_traversal_allow_valid_move('b', 'A8'))
 
-        r = Reversi()
-        r.board = [
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, 'white', 'black', None, None, None],
-            [None, None, None, 'black', 'white', None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None],
-            [None, None, None, None, None, None, None, None]
-        ]
-        self.assertFalse(r.does_west_traversal_allow_valid_move('black', 'G4'))
+        r = Reversi('''
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - w b - - -
+            - - - b w - - -
+            - - - - - - - -
+            - - - - - - - -
+            - - - - - - - -
+            ''')
+        self.assertFalse(r.does_west_traversal_allow_valid_move('b', 'G4'))
 
 if __name__ == '__main__':
     unittest.main()
